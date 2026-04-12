32260f80806239fde8d327388372dbebd7e3c479
Lourdu Radjou🎶 <rajlourdu15@gmail.com>
2026-04-12 08:55:23 +0000

    Add patient page not having alert messages for all required fields

--- components/forms/patient/columns/column-two/fields/SexSelect.tsx
+++ components/forms/patient/columns/column-two/fields/SexSelect.tsx
@@ -6,6 +6,7 @@ import {
     SelectTrigger,
     SelectValue,
 } from '@/components/ui/select'
+import { FormItem, FormControl, FormMessage } from '@/components/ui/form'
 import { AVAILABLE_DISEASES_LIST } from '@/constants/diseases'
 
 type Props = {
@@ -44,27 +45,32 @@ export default function SexSelect({ control }: Props) {
     }
 
     return (
-        <Controller
-            control={control}
-            name="sex"
-            render={({ field }) => (
-                <Select
-                    value={field.value}
-                    onValueChange={(val) => {
-                        field.onChange(val)
-                        clearGenderIncompatible() // ✅ works now
-                    }}
-                >
-                    <SelectTrigger className="w-full" required={true}>
-                        <SelectValue placeholder="Select Sex" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        <SelectItem value="male">Male</SelectItem>
-                        <SelectItem value="female">Female</SelectItem>
-                        <SelectItem value="other">Other</SelectItem>
-                    </SelectContent>
-                </Select>
-            )}
-        />
+        <FormItem>
+            <FormControl>
+                <Controller
+                    control={control}
+                    name="sex"
+                    render={({ field }) => (
+                        <Select
+                            value={field.value}
+                            onValueChange={(val) => {
+                                field.onChange(val)
+                                clearGenderIncompatible() // ✅ works now
+                            }}
+                        >
+                            <SelectTrigger className="w-full" required={true}>
+                                <SelectValue placeholder="Select Sex" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="male">Male</SelectItem>
+                                <SelectItem value="female">Female</SelectItem>
+                                <SelectItem value="other">Other</SelectItem>
+                            </SelectContent>
+                        </Select>
+                    )}
+                />
+            </FormControl>
+            <FormMessage />
+        </FormItem>
     )
 }
