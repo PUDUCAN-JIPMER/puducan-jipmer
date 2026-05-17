42c511a22eaadba73ba435b5c485ae4ffcd8ea0b
PeswikaBavagni-30 <peswikabavagni30@gmail.com>
2026-05-17 20:04:32 +0530

    feat: add Google Maps embed to contact page (#231)
    * feat: add Google Maps embed to contact page
    
    * fix: resolve footer overlap on contact page
    
    * chore: remove unnecessary package lock changes
    
    * fix: correct JIPMER map location and footer layout
    
    * fix: update JIPMER hospital map coordinates
    
    ---------
    
    Co-authored-by: Panduga Maheswar Reddy <panduga.maheswarreddy12@gmail.com>

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,16 +1,16 @@
-// /app/home/layout.tsx
 import HomeNavbar from '@/components/home/HomeNavbar'
 import NavigationLoading from './NavigationLoading'
-import { Suspense } from 'react'
 import Footer from '@/components/ui/footer'
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen flex-col ">
+        <div className="flex min-h-screen flex-col">
             <HomeNavbar />
             <NavigationLoading />
 
-            <main className="p-4 min-h-screen">{children}</main>
+            <main className="flex-1 p-4">
+                {children}
+            </main>
 
             <Footer />
         </div>
