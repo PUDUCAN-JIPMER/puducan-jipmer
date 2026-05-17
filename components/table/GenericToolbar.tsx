53626fbdc107fec68a2a4d2510f6814a43fc7918
Sha-lini3 <yadavshalini7880@gmail.com>
2026-05-17 15:32:24 +0530

    fix: remove undefined keyboard shortcut handler

--- components/table/GenericToolbar.tsx
+++ components/table/GenericToolbar.tsx
@@ -99,7 +99,7 @@ export function GenericToolbar({
             }
         },
 
-        onOpenFilter,
+        
     })
     return (
         <div className="mb-4 flex items-center justify-between">
